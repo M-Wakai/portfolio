12fc60a4f57542be0696ce50ae17091925beb367
admin-PC\pc12 <wakai.minoru.2016@gmail.com>
2017-04-26 17:20:11 +0900

    as of 4/26

--- portfolio/portfolios/js/script.js
+++ portfolio/portfolios/js/script.js
@@ -1,9 +1,53 @@
 $(function(){
     
-    $('#navBtn a').click(function(){
-//        $('#gnav li').css('display','block');
-        $('#gnav li').slideToggle();
+//    $('#navBtn a').click(function(){
+////        $('#gnav li').css('display','block');
+//        $('#gnav li').slideToggle();
+//    });
+//    
+//    return false;
+    
+    var $gnav = $('#gnav_L').offset().top;
+        
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $gnav){
+           $('#gnav_L').css('display','none'); 
+           $('#gnav_S').css('display','block'); 
+           $('#gnav_S').css('position','fixed');   
+           $('#gnav_S').css('top','0'); 
+            $('#gnav_S').css('right','0'); 
+        } else{
+            $('#gnav_S').css('display','none'); 
+            $('#gnav_L').css('display','inline-block');
+            $('#gnav_L').css('position','static'); 
+            $('#gnav_L ul').css('margin','0 auto'); 
+            $('#gnav_L li').css('width','24%');
+            $('#gnav_L li').css('float','left');
+        }
+    });
+});
+
+    $(window).scroll(function () {
+            
+        var $scroll = $(window).scrollTop();
+//        alert($scroll);
+        
+        var $pagetop = $('#pagetop');
+            
+        if ($(window).scrollTop() >= 500) {
+        $('#pagetop').fadeIn(1000);
+        };
     });
     
-    return false;
-});
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 500) {
+        $('#pagetop').fadeOut(1000);
+        };
+    });
+
+    $('#pagetop').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 10000);
+        return false;
+    });
